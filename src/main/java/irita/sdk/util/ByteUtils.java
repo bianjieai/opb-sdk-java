1d0fa1fcde5ec1f07c76eef5cabde2b6cf178f52
congwen <2212007720@qq.com>
2021-11-09 13:22:22 +0800

    getRandomStr

--- src/main/java/irita/sdk/util/ByteUtils.java
+++ src/main/java/irita/sdk/util/ByteUtils.java
@@ -1,6 +1,7 @@
 package irita.sdk.util;
 
 import java.math.BigInteger;
+import java.util.Random;
 
 // copy from web3j
 public class ByteUtils {
@@ -35,4 +36,20 @@ public class ByteUtils {
     }
 
 
+    public static String getRandomStr(int bytes){
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < bytes; i++) {
+            //随机判断判断该字符是数字还是字母
+            String choice = random.nextInt(2) % 2 == 0 ? "char" : "num";
+            if ("char".equalsIgnoreCase(choice)) {
+                //随机判断是大写字母还是小写字母
+                int start = random.nextInt(2) % 2 == 0 ? 65 : 97;
+                sb.append((char) (start + random.nextInt(26)));
+            } else if ("num".equalsIgnoreCase(choice)) {
+                sb.append(random.nextInt(10));
+            }
+        }
+        return sb.toString();
+    }
 }
