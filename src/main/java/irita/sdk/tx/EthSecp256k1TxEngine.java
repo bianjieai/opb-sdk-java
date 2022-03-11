f3f7dd9a540e8cbac08c81224c96e57ecb5c7db4
shiwei <shiwei@bianjie.ai>
2022-03-11 18:13:41 +0800

    update

--- src/main/java/irita/sdk/tx/EthSecp256k1TxEngine.java
+++ src/main/java/irita/sdk/tx/EthSecp256k1TxEngine.java
@@ -10,25 +10,17 @@ import irita.sdk.model.Account;
 import irita.sdk.model.BaseTx;
 import irita.sdk.util.ByteUtils;
 import irita.sdk.util.HashUtils;
-import org.bitcoin.NativeSecp256k1;
-import org.bitcoin.NativeSecp256k1Util;
-import org.bitcoin.Secp256k1Context;
 import org.bitcoinj.core.ECKey;
 import org.bitcoinj.core.Sha256Hash;
-import org.bitcoinj.core.Utils;
-import org.bouncycastle.crypto.digests.KeccakDigest;
-import org.bouncycastle.jcajce.provider.digest.Keccak;
 import org.bouncycastle.math.ec.ECPoint;
 import org.web3j.crypto.ECKeyPair;
 import org.web3j.crypto.Sign;
 import proto.cosmos.base.v1beta1.CoinOuterClass;
-import proto.cosmos.crypto.secp256k1.Keys;
 import proto.cosmos.tx.signing.v1beta1.Signing;
 import proto.cosmos.tx.v1beta1.TxOuterClass;
+import proto.ethermint.crypto.ethsecp256k1.Keys;
 
 import java.math.BigInteger;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
@@ -94,70 +86,11 @@ public class EthSecp256k1TxEngine implements TxEngine {
                 .setChainId(chainID)
                 .build();
 
-        /*byte[] sigData = new byte[64];
-        Sign.SignatureData sig = Sign.signMessage(signDoc.toByteArray(), ECKeyPair.create(privKey),true);
-        System.arraycopy(sig.getR(), 0, sigData, 0, 32);
-        System.arraycopy(sig.getS(), 0, sigData, 32, 32);*/
-       /* byte[] sigData = new byte[64];
-        ECKey ecKey = ECKey.fromPrivate(privKey);
-        byte[] sigData256 = HashUtils.sha256(signDoc.toByteArray());
-        Sha256Hash sha256Hash = Sha256Hash.wrap(sigData256);
-        ECKey.ECDSASignature signature = ecKey.sign(sha256Hash);*/
-        /*System.arraycopy(signature.r.toByteArray(), 0, sigData, 0, 32);
-        System.arraycopy(signature.s.toByteArray(), 0, sigData, 32, 32);*/
-        //ECKey.ECDSASignature ecdsaSignature = new ECKey.ECDSASignature(signature.r,signature.s);
-        //byte[] sigBytes = ByteUtils.addAll(signature.r.toByteArray(), signature.s.toByteArray());
-       /* boolean status = ecKey.verify(sha256Hash,signature);
-        try {
-            boolean statusByte = ecKey.verify(sha256Hash.getBytes(),signature.encodeToDER());
-        } catch (SignatureDecodeException e) {
-            e.printStackTrace();
-        }*/
         byte[] sigData = new byte[64];
-        ECKey ecKey = ECKey.fromPrivate(privKey);
-        MessageDigest digest = null;
-        try {
-            digest = MessageDigest.getInstance( "SHA-256" );
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        byte[] toSignHash = digest.digest(signDoc.toByteArray());
-        ECKey.ECDSASignature Signature = ecKey.sign(Sha256Hash.wrap(toSignHash));
-        System.arraycopy(integerToBytes(Signature.r, 32), 0, sigData, 0, 32);
-        System.arraycopy(integerToBytes(Signature.s, 32), 0, sigData, 32, 32);
-
-       /* Signature signer = null;
-        try {
-            signer = Signature.getInstance("SHA256withECDSA");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        ECKey ecKey = ECKey.fromPrivate(privKey);
-        String pubKey = ecKey.getPublicKeyAsHex();
-        ECPoint ecPoint = CURVE.getCurve().decodePoint(Hex.decode(pubKey));
-        byte[] encoded = ecPoint.getEncoded(false);
-        BigInteger pubKey = new BigInteger(1, Arrays.copyOfRange(encoded, 1, encoded.length));
-        ECKeyPair keyPair = new ECKeyPair(privKey, pubKey);
-        signer.initSign(key);
-        signer.update(data);*/
-        //KeyPair keyPair = KeyPairGenerator.getInstance();
-        /*byte[] sigData256 = HashUtils.sha256(signDoc.toByteArray());
-        try {
-            if (Secp256k1Context.isEnabled()) {
-                byte[] signature = NativeSecp256k1.sign(sigData256, Utils.bigIntegerToBytes(privKey, 32));
-            }
-
-        } catch (NativeSecp256k1Util.AssertFailException e) {
-            e.printStackTrace();
-        }*/
-        /*byte[] sigData = new byte[64];
         BigInteger pubKey = Sign.publicKeyFromPrivate(privKey);
         ECKeyPair keyPair = new ECKeyPair(privKey, pubKey);
         Sign.SignatureData signature = Sign.signMessage(HashUtils.sha256(signDoc.toByteArray()), keyPair, false);
-        sigData = ByteUtils.addAll(signature.getR(), signature.getS());*/
-
-
-
+        sigData = ByteUtils.addAll(signature.getR(), signature.getS());
         return TxOuterClass.Tx.newBuilder()
                 .setBody(txBody)
                 .setAuthInfo(ai)
--- src/test/java/irita/sdk/MsgsDemoTest.java
+++ src/test/java/irita/sdk/MsgsDemoTest.java
@@ -31,12 +31,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MsgsDemoTest {
     private IritaClient client;
-    private final BaseTx baseTx = new BaseTx(200000, new Fee("200000", "ugas"), BroadcastMode.Commit);
+    private final BaseTx baseTx = new BaseTx(200000, new Fee("200000", "uirita"), BroadcastMode.Commit);
 
     @BeforeEach
     public void init() {
         //更换为自己链上地址的助记词
-        String mnemonic = "edit endorse silver canyon silver cargo draft almost worth snack twin predict hint orange thumb apology same end weapon bitter tip shy elevator already";
+        String mnemonic = "leisure timber expire affair wife solar combine mercy language amount bacon merge lottery rural rule bench job pattern rule another copper method slender police";
         KeyManager km = KeyManagerFactory.createDefault();
         km.recover(mnemonic);
 
@@ -44,7 +44,7 @@ public class MsgsDemoTest {
         //连接测试网（连接主网请参考README.md）
         String nodeUri = "http://127.0.0.1:26657";
         String grpcAddr = "127.0.0.1:9090";
-        String chainId = "irita-test";
+        String chainId = "irita-tesnet";
         /*System.getProperty("java.library.path");
         System.loadLibrary("libsecp256k1");*/
 
@@ -54,7 +54,7 @@ public class MsgsDemoTest {
 
         client = new IritaClient(clientConfig, opbConfig, km);
         //判断由助记词恢复的是否为预期的链上地址
-        assertEquals("iaa1tu4v8q40nwxkhthverngnzqeyl47ay3ux9932n", km.getCurrentKeyInfo().getAddress());
+        assertEquals("iaa14cn0hyqnez0f9v5n8lclewegmss8ysxc2u0qqs", km.getCurrentKeyInfo().getAddress());
     }
 
     @Test
