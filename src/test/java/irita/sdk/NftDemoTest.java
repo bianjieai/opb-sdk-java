d491ca92d9437ba02006f26439b4a3f5c6f25b13
shiwei <shiwei@bianjie.ai>
2022-06-20 10:06:24 +0800

    Modify notes

--- src/test/java/irita/sdk/NftDemoTest.java
+++ src/test/java/irita/sdk/NftDemoTest.java
@@ -65,8 +65,8 @@ public class NftDemoTest {
         String schema = "no shcema";
         String symbol = "symbol";
         //是否限制发行
-        //true  只有 Denom 的所有者可以发行此类别的 NFT 给自己（发行到别的地址也是不可以的）
-        //false 任何人都可以发行 NFT
+        //true 只有 Denom 的所有者可以发行此类别的 NFT
+        //false 任何人都可以发行 NFT只有 Denom 的所有者可以发行此类别
         boolean mintRestricted = false;
         //是否限制更新 NFT
         //true  任何人都不可以更新 NFT
