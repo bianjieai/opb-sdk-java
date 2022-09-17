df753050995010448baeee22fa0d8a996b75cd1d
shiwei <shiwei@bianjie.ai>
2022-09-17 18:44:31 +0800

    add query FeeGrant information

--- src/main/java/irita/sdk/module/feegrant/FeeGrantClient.java
+++ src/main/java/irita/sdk/module/feegrant/FeeGrantClient.java
@@ -3,13 +3,17 @@ package irita.sdk.module.feegrant;
 import com.google.protobuf.Any;
 import com.google.protobuf.GeneratedMessageV3;
 import com.google.protobuf.Timestamp;
+import io.grpc.ManagedChannel;
 import irita.sdk.client.BaseClient;
 import irita.sdk.model.Account;
 import irita.sdk.model.BaseTx;
 import irita.sdk.model.ResultTx;
 import org.apache.commons.lang3.StringUtils;
+import proto.cosmos.base.query.v1beta1.Pagination;
 import proto.cosmos.base.v1beta1.CoinOuterClass;
 import proto.cosmos.feegrant.v1beta1.Feegrant;
+import proto.cosmos.feegrant.v1beta1.QueryGrpc;
+import proto.cosmos.feegrant.v1beta1.QueryOuterClass;
 import proto.cosmos.feegrant.v1beta1.Tx;
 
 import java.io.IOException;
@@ -91,6 +95,35 @@ public class FeeGrantClient {
 
         List<GeneratedMessageV3> msgs = Collections.singletonList(msg);
         return baseClient.buildAndSend(msgs, baseTx, account);
+    }
+
+    public QueryOuterClass.QueryAllowanceResponse Allowance(String granter, String grantee){
+        ManagedChannel channel = baseClient.getGrpcClient();
+        QueryOuterClass.QueryAllowanceRequest request = QueryOuterClass.QueryAllowanceRequest
+                .newBuilder()
+                .setGranter(granter)
+                .setGrantee(grantee)
+                .build();
+        QueryOuterClass.QueryAllowanceResponse response = QueryGrpc.newBlockingStub(channel).allowance(request);
+        channel.shutdown();
+        return response;
+    }
+
+    public QueryOuterClass.QueryAllowancesResponse Allowances (String grantee, Pagination.PageRequest page){
+        ManagedChannel channel = baseClient.getGrpcClient();
+        if (page == null) {
+            page = Pagination.PageRequest.newBuilder()
+                    .setOffset(0)
+                    .setLimit(100)
+                    .build();
+        }
+        QueryOuterClass.QueryAllowancesRequest request = QueryOuterClass.QueryAllowancesRequest.newBuilder()
+                .setGrantee(grantee)
+                .setPagination(page)
+                .build();
+        QueryOuterClass.QueryAllowancesResponse response = QueryGrpc.newBlockingStub(channel).allowances(request);
+        channel.shutdown();
+        return response;
     }
 
 }
