5cc6e1726327421ac78a09f45016cee170fada1d
sxj <a550374@163.com>
2022-10-18 18:00:53 +0800

    KeyManager新增方法，通过变换派生路径实现一个mnemonic生成多个链账户

--- src/main/java/irita/sdk/key/KeyManager.java
+++ src/main/java/irita/sdk/key/KeyManager.java
@@ -17,6 +17,7 @@ import java.security.cert.CertificateException;
 public abstract class KeyManager implements Key, MultiKey {
     // this keyPath and hrp just for iris
     private String keyPath = "m/44'/118'/0'/0/0";
+    private String hdPath = "m/44'/118'/0'/0/";
     private String hrp = "iaa";
     private KeyInfo currentKeyInfo;
     protected final KeyDAO keyDAO = new MemoryKeyDAO();
@@ -78,8 +79,7 @@ public abstract class KeyManager implements Key, MultiKey {
     @Override
     public void recover(String mnemonic, int index) {
         byte[] seed = Bip44Utils.getSeed(mnemonic);
-
-        String keyPath = getKeyPath() + String.valueOf(index);
+        String keyPath = hdPath + index;
         DeterministicKey dk = Bip44Utils.getDeterministicKey(mnemonic, seed, keyPath);
         BigInteger privKey = dk.getPrivKey();
         recover(privKey);
